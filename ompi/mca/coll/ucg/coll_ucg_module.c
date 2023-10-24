5b708ccc4eba1472bff9604b8fe6bb64a699b5c8
1091400761@qq.com <1091400761@qq.com>
2023-10-24 19:57:51 +0800

    [Huawei] COLL/UCX: close gatherv/igatherv interface

--- ompi/mca/coll/ucg/coll_ucg_module.c
+++ ompi/mca/coll/ucg/coll_ucg_module.c
@@ -492,7 +492,7 @@ static void mca_coll_ucg_module_construct(mca_coll_ucg_module_t *module)
         MCA_COLL_UCG_SET_CACHE_HANDLER(bcast);
         MCA_COLL_UCG_SET_CACHE_HANDLER(alltoallv);
         MCA_COLL_UCG_SET_CACHE_HANDLER(scatterv);
-        MCA_COLL_UCG_SET_CACHE_HANDLER(gatherv);
+        // MCA_COLL_UCG_SET_CACHE_HANDLER(gatherv);
         MCA_COLL_UCG_SET_CACHE_HANDLER(allgatherv);
 
         MCA_COLL_UCG_SET_CACHE_HANDLER(iallreduce);
@@ -500,7 +500,7 @@ static void mca_coll_ucg_module_construct(mca_coll_ucg_module_t *module)
         MCA_COLL_UCG_SET_CACHE_HANDLER(ibcast);
         MCA_COLL_UCG_SET_CACHE_HANDLER(ialltoallv);
         MCA_COLL_UCG_SET_CACHE_HANDLER(iscatterv);
-        MCA_COLL_UCG_SET_CACHE_HANDLER(igatherv);
+        // MCA_COLL_UCG_SET_CACHE_HANDLER(igatherv);
         MCA_COLL_UCG_SET_CACHE_HANDLER(iallgatherv);
     } else {
         MCA_COLL_UCG_SET_HANDLER(allreduce);
@@ -508,7 +508,7 @@ static void mca_coll_ucg_module_construct(mca_coll_ucg_module_t *module)
         MCA_COLL_UCG_SET_HANDLER(bcast);
         MCA_COLL_UCG_SET_HANDLER(alltoallv);
         MCA_COLL_UCG_SET_HANDLER(scatterv);
-        MCA_COLL_UCG_SET_HANDLER(gatherv);
+        // MCA_COLL_UCG_SET_HANDLER(gatherv);
         MCA_COLL_UCG_SET_HANDLER(allgatherv);
 
         MCA_COLL_UCG_SET_HANDLER(iallreduce);
@@ -516,7 +516,7 @@ static void mca_coll_ucg_module_construct(mca_coll_ucg_module_t *module)
         MCA_COLL_UCG_SET_HANDLER(ibcast);
         MCA_COLL_UCG_SET_HANDLER(ialltoallv);
         MCA_COLL_UCG_SET_HANDLER(iscatterv);
-        MCA_COLL_UCG_SET_HANDLER(igatherv);
+        // MCA_COLL_UCG_SET_HANDLER(igatherv);
         MCA_COLL_UCG_SET_HANDLER(iallgatherv);
     }
 
@@ -525,7 +525,7 @@ static void mca_coll_ucg_module_construct(mca_coll_ucg_module_t *module)
     MCA_COLL_UCG_SET_HANDLER(bcast_init);
     MCA_COLL_UCG_SET_HANDLER(alltoallv_init);
     MCA_COLL_UCG_SET_HANDLER(scatterv_init);
-    MCA_COLL_UCG_SET_HANDLER(gatherv_init);
+    // MCA_COLL_UCG_SET_HANDLER(gatherv_init);
     MCA_COLL_UCG_SET_HANDLER(allgatherv_init);
     return;
 }
